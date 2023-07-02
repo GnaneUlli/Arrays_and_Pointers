fe7a301d1a32ed2f9042ea60cd3ea01cb36b9d0c
Ulli Gnaneswararao <133844134+Gnaneswar484@users.noreply.github.com>
2023-07-02 19:54:00 +0530

    Update Matrix_multi.c

--- Matrix_multi.c
+++ Matrix_multi.c
@@ -3,7 +3,6 @@
 #define MAX_SIZE 10
 
 void read(int matrix[MAX_SIZE][MAX_SIZE], int rows, int columns) {
-    
     printf("Enter the elements of the matrix:\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
